2d536a520794d726929d3926c0a0398583a464d1
annick975 <niyubahwe.annick975@gmail.com>
2024-08-10 18:36:36 -0700

    Conditional Statements

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,28 @@
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
+        // Create a Scanner object to read input from the terminal
         Scanner scanner = new Scanner(System.in);
-        System.out.print("How old are you?");
-       String age =  scanner.next();
-       System.out.println("I 'm " +age +" Years old!");
 
-       String result = NumberFormat.getPercentInstance().format(5.6);
-       System.out.println(result);
+        // Prompt the user to enter the temperature
+        System.out.print("Enter the temperature in degrees Celsius: ");
+        int temperature = scanner.nextInt();
+
+        // Check the temperature and give advice
+        if (temperature > 30) {
+            System.out.println("It's hot! Don't forget to drink plenty of water.");
+        } else if (temperature >= 20 && temperature <= 30) {
+            System.out.println("The weather is nice. Enjoy your day!");
+        } else if (temperature >= 10 && temperature < 20) {
+            System.out.println("It's a bit cool. You might want to wear a light jacket.");
+        } else if (temperature >= 0 && temperature < 10) {
+            System.out.println("It's cold! Make sure to bundle up.");
+        } else {
+            System.out.println("It's freezing! Stay warm and be careful.");
+        }
+
+        // Close the scanner
+        scanner.close();
     }
-
 }
-
-
